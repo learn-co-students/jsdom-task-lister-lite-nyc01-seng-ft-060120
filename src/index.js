cbd3a46e2210e8cd1aec10e7d8e4be975068f029
mstaso <mstaso1@gmail.com>
2020-07-14 17:29:44 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  // const taskList = new taskList()
+  const newTaskForm = document.getElementById('create-task-form')
+  const newTaskDescription = document.getElementById('new-task-description')
+  const taskContainer = document.getElementById('tasks')
+newTaskForm.addEventListener('submit', newTask);
+function newTask(event){
+  event.preventDefault();
+  const newLi = document.createElement('li')
+  newLi.className = 'lists'
+  const btn = document.createElement('button')
+  btn.id = 'buttonRemove'
+  newLi.className = 'newItems'
+  newLi.innerText = newTaskDescription.value
+  taskContainer.append(newLi)
+  taskContainer.append(btn)
+  event.target.reset()
+}
