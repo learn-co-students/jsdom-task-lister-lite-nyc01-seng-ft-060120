f387f1e35d629501a282bd54c9bd69bc6cb3cf58
shadman-a <shadman@outlook.com>
2020-07-14 16:33:50 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const taskForm = document.getElementById("create-task-form")
+  taskForm.addEventListener("submit",function(e){
+    e.preventDefault()
+    let newTask = document.getElementById('new-task-description').value
+    
+    const ul  = document.getElementById("tasks")
+    const li = document.createElement('li')
+    ul.appendChild(li);
+    li.innerText = newTask
+    taskForm.reset();
+  })
+  
+
+
 });
+  
+
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+
+// let button = document.getElementById('create-new-form')
+// button.addEventListener('submit', function(e){
+// e.preventDefault()
+// let newTask = document.getElementById('new-task-description').value
+// const ul = document.getElementById('tasks');
+// const li = document.createElement('li')
+// li.appendChild(document.createTextNode(newTask));
+// ul.appendChild(li);
+// let form = document.getElementById('create-task-form').reset();
+// })
+
+// });
