a66d2a2274ed2a3df5eb61a1608b2ec8796fca99
scubaguba345 <brandon.gubitosa@gmail.com>
2020-07-14 16:59:34 -0400

    deliverable done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  let tasks = document.getElementById("tasks");
+  let newTaskDescription = document.getElementById("new-task-description");
+  let createTaskForm = document.getElementById("create-task-form");
+
+  createTaskForm.addEventListener("submit", function(event){
+     let newMessage = document.createElement("li");
+     newMessage.innerHTML = newTaskDescription.value;
+     tasks.appendChild(newMessage);
+     newTaskDescription.value = "";
+     //event.target.reset();
+     event.preventDefault();
+  })
+})
+
+
