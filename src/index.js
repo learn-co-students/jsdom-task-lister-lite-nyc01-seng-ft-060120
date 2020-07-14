9970ace59d5bb00bb59482cbe2d84362de721ae0
MaleehaBhuiyan <maleehabhuiyan@baruchmail.cuny.edu>
2020-07-14 16:41:14 -0400

    :)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", function(){
+
+  console.log("The Dom has Loaded")
+  
 });
+
+
+
+const submit = document.getElementById('submit');
+
+submit.addEventListener("click", function(e){
+  e.preventDefault();
+  console.log(e.target);
+});
+
+
+const newTaskDescription = document.GetElementById("new-task-description")
+
+function addTask(task){
+  const taskLi = document.createElement("li")
+  taskLi.className = "tasks"
+  taskLi.innerHTML = newTaskDescription.value
+  newTaskDescription.append(taskLi)
+}
+
+
+
+
+// function addTask(taskObject){
+//   const taskLi = document.createElement("li")
+//   taskLi.className = 'task'
+//   taskLi.innerHTML = "Hi"
+//   tasks.append(taskLi)
+// }
+
+
+
+
